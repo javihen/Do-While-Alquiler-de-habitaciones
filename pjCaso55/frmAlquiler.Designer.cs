a583caa505dd09c369264b0b96740d4ea864686c
javier <javi_hen@hotmail.com>
2020-10-14 11:45:13 -0400

    Agregar archivos de proyecto.

---
+++ pjCaso55/frmAlquiler.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace pjCaso55
+{
+    partial class frmAlquiler
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lvAlquiler = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tHora = new System.Windows.Forms.Timer(this.components);
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO DE ALQUILER DE HABITACIONES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "FECHA";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(78, 57);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(63, 15);
+            this.lblFecha.TabIndex = 0;
+            this.lblFecha.Text = "lblFecha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(175, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "HORA";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(241, 57);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(56, 15);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "lblHora";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 15);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "TIPO DE HABITACION";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Simple",
+            "Doble",
+            "Matrimonial",
+            "Presidencial"});
+            this.cboTipo.Location = new System.Drawing.Point(156, 27);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(194, 23);
+            this.cboTipo.TabIndex = 2;
+            this.cboTipo.Text = "(Seleccione tipo)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.txtDias);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cboTipo);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(619, 62);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATOS DEL ALQUILER";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(485, 20);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(119, 34);
+            this.btnRegistrar.TabIndex = 4;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtDias
+            // 
+            this.txtDias.Location = new System.Drawing.Point(406, 27);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(62, 23);
+            this.txtDias.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(368, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "DIAS";
+            // 
+            // lvAlquiler
+            // 
+            this.lvAlquiler.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvAlquiler.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvAlquiler.GridLines = true;
+            this.lvAlquiler.HideSelection = false;
+            this.lvAlquiler.Location = new System.Drawing.Point(12, 174);
+            this.lvAlquiler.Name = "lvAlquiler";
+            this.lvAlquiler.Size = new System.Drawing.Size(616, 135);
+            this.lvAlquiler.TabIndex = 4;
+            this.lvAlquiler.UseCompatibleStateImageBehavior = false;
+            this.lvAlquiler.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TIPO DE HABITACION";
+            this.columnHeader1.Width = 157;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "COSTO POR DIA";
+            this.columnHeader2.Width = 107;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "DIAS";
+            this.columnHeader3.Width = 54;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "SUBTOTAL";
+            this.columnHeader4.Width = 101;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "DESCUENTO";
+            this.columnHeader5.Width = 91;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "PAGO NETO";
+            this.columnHeader6.Width = 99;
+            // 
+            // lvEstadisticas
+            // 
+            this.lvEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lvEstadisticas.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvEstadisticas.GridLines = true;
+            this.lvEstadisticas.HideSelection = false;
+            this.lvEstadisticas.Location = new System.Drawing.Point(12, 332);
+            this.lvEstadisticas.Name = "lvEstadisticas";
+            this.lvEstadisticas.Size = new System.Drawing.Size(616, 135);
+            this.lvEstadisticas.TabIndex = 4;
+            this.lvEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lvEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "TIPO DE HABITACION";
+            this.columnHeader7.Width = 196;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "TOTAL DIAS";
+            this.columnHeader8.Width = 123;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "NETO ACUMULADO";
+            this.columnHeader9.Width = 157;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(18, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "REGISTRO DE ALQUILER";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 312);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 15);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "ESTADISTICAS";
+            // 
+            // tHora
+            // 
+            this.tHora.Tick += new System.EventHandler(this.tHora_Tick);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(12, 473);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmAlquiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 504);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lvEstadisticas);
+            this.Controls.Add(this.lvAlquiler);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAlquiler";
+            this.Text = "Control de alquiler de habitaciones";
+            this.Load += new System.EventHandler(this.frmAlquiler_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView lvAlquiler;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ListView lvEstadisticas;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer tHora;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ pjCaso55/frmAlquiler.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjCaso55
+{
+    public partial class frmAlquiler : Form
+    {
+        public frmAlquiler()
+        {
+            InitializeComponent();
+            tHora.Enabled = true;
+        }
+
+        private void frmAlquiler_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void tHora_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Capturar los datos
+            string habitacion = cboTipo.Text;
+            int dias = int.Parse(txtDias.Text);
+
+            //Asignacion de costo por habitacion 
+            double costo = 0;
+            switch (habitacion)
+            {
+                case "Simple": costo = 30; break;
+                case "Doble": costo = 50; break;
+                case "Matrimonial": costo = 120; break;
+                case "Presidencial": costo = 300; break;
+            }
+
+            //Realizando los calculos
+            double subtotal = costo * dias;
+
+            double descuento = 0;
+            if (dias <= 5)
+                descuento = 0;
+            else if (dias > 5 && dias <= 10)
+                descuento = 0.05 * subtotal;
+            else if (dias > 10 && dias <= 30)
+                descuento = 0.1 * subtotal;
+            else if (dias > 30)
+                descuento = 0.2 * subtotal;
+
+            double neto = subtotal - descuento;
+            
+            //Enviar los datos al registro de alquiler
+            ListViewItem fila = new ListViewItem(habitacion);
+            fila.SubItems.Add(costo.ToString("0.00"));
+            fila.SubItems.Add(dias.ToString());
+            fila.SubItems.Add(subtotal.ToString("0.00"));
+            fila.SubItems.Add(descuento.ToString("0.00"));
+            fila.SubItems.Add(neto.ToString("0.00"));
+            lvAlquiler.Items.Add(fila);
+
+            //Determinar las estadisticas 
+            int i = 0;
+            int cSimple = 0, cDoble = 0, cMatrimonial = 0, cPresidencial = 0;
+            double tSimple = 0, tDoble = 0, tMatrimonial = 0, tPresidencial = 0;
+
+            do {
+                string tipo = lvAlquiler.Items[i].SubItems[0].Text;
+                switch (tipo)
+                {
+                    case "Simple":
+                        cSimple += int.Parse(lvAlquiler.Items[i].SubItems[2].Text);
+                        tSimple+= double.Parse(lvAlquiler.Items[i].SubItems[5].Text);
+                        break;
+                    case "Doble":
+                        cDoble += int.Parse(lvAlquiler.Items[i].SubItems[2].Text);
+                        tDoble += double.Parse(lvAlquiler.Items[i].SubItems[5].Text);
+                        break;
+
+                    case "Matrimonial":
+                        cMatrimonial += int.Parse(lvAlquiler.Items[i].SubItems[2].Text);
+                        tMatrimonial += double.Parse(lvAlquiler.Items[i].SubItems[5].Text);
+                        break;
+                    case "Presidencial":
+                        cPresidencial += int.Parse(lvAlquiler.Items[i].SubItems[2].Text);
+                        tPresidencial += double.Parse(lvAlquiler.Items[i].SubItems[5].Text);
+                        break;
+                }
+                i++;
+            } while (i<lvAlquiler.Items.Count);
+
+            //Imprimir las estadisticas
+            //Imprimiendo las estadisticas
+            lvEstadisticas.Items.Clear();
+            string[] elementosFila = new string[3];
+            ListViewItem row;
+
+            elementosFila[0] = "Habitacion Simple";
+            elementosFila[1] = cSimple.ToString();
+            elementosFila[2] = tSimple.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Habitacion Doble";
+            elementosFila[1] = cDoble.ToString();
+            elementosFila[2] = tDoble.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Habitacion Matrimonial";
+            elementosFila[1] = cMatrimonial.ToString();
+            elementosFila[2] = tMatrimonial.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Habitacion Presidencial";
+            elementosFila[1] = cPresidencial.ToString();
+            elementosFila[2] = tPresidencial.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?",
+                                  "Seguros",
+                                  MessageBoxButtons.YesNo,
+                                  MessageBoxIcon.Exclamation);
+            if (r == DialogResult.Yes) this.Close();
+        }
+    }
+}
